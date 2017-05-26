45be5d76b0788965024caeeda03d7c0efac641b1
Ivan Porta <ivan.porta.web@gmail.com>
2017-05-27 01:28:02 +0200

    Third CMS partial commit

---
+++ CMS 1.0.2/language/italian/italian_lang.php
@@ -0,0 +1,197 @@
+<?php
+/* TOP NAVIGATION */
+$lang["top_nav_welcome_shop"] = "Benvenuto nel nostro negozio online!";
+$lang["top_nav_welcome"] = "Benvenuto";
+$lang["top_nav_signin"] = "Accedi";
+$lang["top_nav_or"] = "oppure";
+$lang["top_nav_register"] = "Registrati";
+$lang["top_nav_my_bag"] = "Carrello";
+$lang["top_nav_my_bag_2"] = "Pagamento";
+$lang["top_nav_account"] = "Account";
+$lang["top_nav_account_1"] = "Il Mio Account";
+$lang["top_nav_account_2"] = "Accedi / Registrati";
+$lang["top_nav_account_3"] = "Lista dei Desideri";
+$lang["top_nav_currency"] = "Valuta";
+$lang["top_nav_language"] = "Lingua";
+$lang["top_nav_search"] = "Ricerca";
+$lang["top_nav_search_placeholder"] = "Scrivi qui";
+/* NAVIGATION */
+$lang["nav_home"] = "Home";
+$lang["nav_about"] = "Chi Siamo";
+$lang["nav_manufacturers"] = "Produttori";
+$lang["nav_sales"] = "Saldi";
+$lang["nav_blog"] = "Blog";
+$lang["nav_contacts"] = "Contatti";
+/* LEFT NAVIGATION */
+/* PRODOTTI */
+$lang["left_nav_category"] = "Categoria";
+$lang["left_nav_shop_by"] = "Compra per";
+$lang["left_nav_manufacturer"] = "Produttori";
+$lang["left_nav_price"] = "Prezzo";
+$lang["left_nav_filter"] = "Filtra";
+/* PRODOTTO */
+$lang["left_nav_related_product"] = "Prodotti correlati";
+/* BLOG */
+$lang["left_nav_recent_post"] = "Post Recenti";
+$lang["left_nav_archive"] = "Archivio";
+$lang["left_nav_gallery"] = "Galleria";
+/* ACCOUNT */
+$lang["left_nav_profile"] = "Profilo";
+$lang["left_nav_my_order"] = "I miei oridni";
+$lang["left_nav_logout"] = "Disconnetti";
+/* GENERAL */
+$lang["left_nav_tags"] = "Tags";
+$lang["left_nav_show_more"] = "Mostra di più";
+$lang["left_nav_read_more"] = "Leggi di più";
+
+/* INDEX */
+$lang["index_special_deals"] = "Offerte speciali";
+$lang["index_new_products"] = "Nuovi prodotti";
+$lang["index_bestsellers"] = "I più venduti";
+$lang["index_featured_items"] = "Prodotti in primo piano";
+$lang["index_latest_blog"] = "Ultime dal blog";
+$lang["index_products_new"] = "Nuovo";
+$lang["index_products_view_more"] = "Mostra di più";
+$lang["index_products_all"] = "Tutto";
+
+$lang["index_box_title_1"] = "Spedizione gratuita";
+$lang["index_box_text_1"] = "FashionLux.it garantisce la spedizione gratuita in tutti i paesi del mondo fino al 1 Aprile 2016";
+$lang["index_box_title_2"] = "Servizio clienti";
+$lang["index_box_text_2"] = "Servizio volto a cercare le soluzioni più appropriate alle vostre esigenze, aggiornarvi sulle nostre promozioni, novità e modelli.";
+$lang["index_box_title_3"] = "Soddisfatti o rimborsati";
+$lang["index_box_text_3"] = "Un rimborso completo sarà assicurato per tutti i  clienti che  entro  15 giorni dal ricevimento della merce";
+
+/* ABOUT */
+$lang["about_title_1"] = "La nostra storia";
+$lang["about_title_2"] = "Servizio clienti";
+$lang["about_text_1"] = "<p>Il sito web propone le collezioni per uomo e per donna dei più prestigiosi fashion designer quali Valentino Garavani, Prada , Miu Miu , Church's , Chloé, Dior, Dsquared, Dolce & Gabbana, Givenchy, Lanvin, Rick Owens, Roberto Cavalli, Saint Laurent... </p><p>Grazie a rapporti diretti e consolidati con i dealer autorizzati, FASHIONLUX.IT ti offre una selezione di prodotti di altissima qualità e dall’autenticità garantita. Prima di essere messi in vendita, tutti i prodotti  vengono sottoposti a scrupolosi controlli per garantire che il prodotto risponda ai nostri elevati standard qualitativi.</p>";
+$lang["about_text_2"] = "<p>Tutti gli ordini partono dalla sede di FASHIONLUX a Fara Gera d'Adda all’interno di confezioni preparate con la massima cura utilizzando i migliori materiali e con attenzione ai dettagli.</p><p>l nostro Servizio Clienti è disponibile a rispondere a tutte le domande, risolvere problemi e offrire assistenza e consigli di moda a tutti I clienti. I nostri articoli sono spediti in tutto il mondo.</p>";
+/* PRODUCT */
+$lang["products_quantity"] = "QTY";
+$lang["products_reviews"] = "Recensioni";
+$lang["products_share_this"] = "Condividi su";
+$lang["products_description"] = "Descrizione";
+$lang["products_details"] = "Dettagli";
+$lang["products_other_products"] = "Altri Prodotti";
+$lang["products_add_cart"] = "Aggiungi al carrello";
+$lang["products_ean"] = "EAN";
+$lang["products_sku"] = "SKU";
+$lang["products_manufacturer"] = "Produttore";
+$lang["products_material"] = "Materiale";
+$lang["products_application"] = "Applicazioni";
+$lang["products_style"] = "Stile";
+$lang["products_plateau_height"] = "Altezza Plateau";
+$lang["products_sole_material"] = "Materiale Suola";
+$lang["products_sole_support_material"] = "Materiale Soletta";
+$lang["products_width_bag"] = "Lunghezza Borsa";
+$lang["products_height_bag"] = "Altezza Borsa";
+$lang["products_lenght_bag"] = "Larghezza Borsa";
+$lang["products_shoulder_bag"] = "Tracolla";
+$lang["products_shoulder_bag_present"] = "Presente";
+$lang["products_shoulder_bag_not_present"] = "Non Present";
+$lang["products_removible_bag"] = "Rimovibile";
+$lang["products_removible_bag_yes"] = "Si";
+$lang["products_removible_bag_no"] = "No";
+$lang["products_closign_type_bag"] = "Tipo di chiusura";
+$lang["products_lining_bag"] = "Fodera";
+$lang["products_wearability"] = "Vestibilità";
+$lang["product_quantity_updated"] = "Quantità del prodotto aggiornata";
+$lang["product_quanity_max_available"] = "Quantità disponibile raggiunta";
+$lang["product_added"] = "Prodotto aggiunto al carrello";
+/* BLOG */
+$lang["blog_view_more"] = "Mostra di più";
+/* BLOG_SINGLE */
+$lang["blog_single_post_by"] = "Pubblicato da";
+/* LOGIN / REGISTRATION */
+$lang["login_already_registered"] = "Sei già registrato? Accedi con i tuoi dati";
+$lang["login_already_registered_2"] = "Sei già registrato?";
+$lang["login_write_email"] = "Scrivi la tua email";
+$lang["login_write_password"] = "Scrivi la tua password";
+$lang["login_forgot_password"] = "Hai dimenticato la password?";
+$lang["login_click_here"] = "Clicca qui...";
+$lang["login_not_registered"] = "Non sei ancora registrato?";
+$lang["login_email"] = "Email";
+$lang["login_repeat_email"] = "Ripeti la Email";
+$lang["login_password"] = "Password";
+$lang["login_repeat_password"] = "Ripeti la Password";
+$lang["login_name"] = "Nome";
+$lang["login_surname"] = "Cognome";
+$lang["login_country"] = "Nazione";
+$lang["login_state"] = "Provincia";
+$lang["login_city"] = "Città";
+$lang["login_zip"] = "CAP";
+$lang["login_address"] = "Indirizzo";
+$lang["login_house_number"] = "Numero Civico";
+$lang["login_phone"] = "Telefono";
+$lang["login_privacy_note"] = "Per noi la protezione della tua privacy è importante. Per questo, utilizziamo i tuoi dati personali rispettando attentamente le direttive di legge. I tuoi dati personali sono trattati esclusivamente per finalità strettamente legate al nostro sito. Maggiori info in riferimento al trattamento dei dati personali vengono descritte alla pagina ";
+$lang["login_privacy"] = "Privacy";
+$lang["login_newsletter_note"] = "Iscriviti alla newsletter per rimanere aggiornato sulle novità e ricevere le offerte in anteprima. I tuoi dati sono riservati e puoi annullare l’iscrizione in qualsiasi momento.";
+$lang["login_recover_password"] = "Recupera la password";
+$lang["login_recover_password_description"] = "Inserisci la tua email di registrazione e ti invieremo le istruzioni per ripristinare l'accesso al tuo account: ";
+$lang["login_login"] = "Login";
+$lang["login_close"] = "Chiudi";
+/* PRIVACY */
+$lang["privacy_policy"] = "Informativa sulla privacy";
+$lang["privacy_description"] = "<h5>Informativa sulla privacy ex Art. 13 del Decreto Legislativo 196 del 2003</h5><p>Fashionlux tratterà i dati personali raccolti con la compilazione del modulo di registrazione con modalità, anche automatizzate, per l’esecuzione degli ordini di acquisto nonché per l’invio, via e-mail e per posta ordinaria, di informazioni commerciali e pubblicitarie relative a prodotti e servizi analoghi a quelli acquistati dal cliente. Quest’ultimo potrà opporsi al trattamento dei dati per finalità commerciali e pubblicitarie, inviando una e-mail a cancellami, scrivendo all’indirizzo sotto indicato ovvero contattandoci telefonicamente: Tel. +39 0363 39 79 82.</p><p>Il conferimento dei dati è facoltativo; tuttavia, qualora non venissero compilati i campi contrassegnati come obbligatori (indicati con *), tutti o alcuni dei fini indicati non potranno essere perseguiti.</p><p>Con il consenso dell’interessato, potrà essere utilizzato il numero di fax e il numero di telefono per l’invio di materiale pubblicitario, informazioni commerciali e la vendita diretta.</p><p>Sempre con il consenso dell’interessato, i dati personali raccolti potranno essere trattati da Fashionlux.it per analizzare abitudini e scelte di consumo dei clienti.</p><p>I dati saranno trattati dal personale di Fashionlux, debitamente nominato incaricato del trattamento, e dalle società che prestano attività strumentali o accessorie, designate da Savemybag.it responsabili del trattamento. I dati verranno, inoltre, comunicati agli spedizionieri di cui si avvale Fashionlux.it per la consegna della merce.</p><p>Titolare del trattamento è Fashionlux, Via Giuseppe verdi 7/A - 24045 Fara Gera d'Adda, Bergamo, Italia. Per l’esercizio dei diritti di cui all’art. 7 del D.Lgs. 196/2003 (accesso, correzione, integrazione, cancellazione, opposizione al trattamento ecc.) potete rivolgerVi al responsabile del trattamento, scrivendo all’indirizzo sopraindicato ovvero inviando una e-mail a modifica Presso quest’ultimo è consultabile l’elenco degli altri responsabili del trattamento nominati e degli eventuali terzi ai quali possono venir comunicati i dati.</p>";
+/* ACCOUNT */
+$lang["account_title_1"] = "I miei dati";
+$lang["account_title_2"] = "I miei ordini";
+$lang["account_description_2"] = "Ecco la lista dei tuoi ordini. In questa pagina puoi vedere il riepilogo dei tuoi ordini, lo stato dei tuoi acquisti, la data di ordine e il totale. Per i dettagli del singolo ordine, clicca su 'Dettaglio'.";
+$lang["account_find"] = "Trovati";
+$lang["account_orders"] = "Ordini";
+$lang["account_order_number"] = "nr. ordine";
+$lang["account_order_data"] = "data";
+$lang["account_order_status"] = "stato dell’ordine";
+$lang["account_order_total"] = "totale";
+$lang["account_order_details"] = "Dettaglio";
+$lang["account_name"] = "Nome";
+$lang["account_surname"] = "Cognome";
+$lang["account_nation"] = "Nazione";
+$lang["account_province"] = "Provincia";
+$lang["account_city"] = "Città";
+$lang["account_zip"] = "CAP";
+$lang["account_house_number"] = "Numero Civico";
+$lang["account_phone"] = "Telefono";
+$lang["account_save"] = "Salva";
+$lang["account_resume_order"] = "Riepilogo ordine";
+/* CONTATTACI */
+$lang["contact_title"] = "Lasciaci un messaggio";
+$lang["contact_placeholder_name"] = "Il tuo Nome";
+$lang["contact_placeholder_email"] = "La tua Email";
+$lang["contact_placeholder_phone"] = "Recapito Telefonico";
+$lang["contact_placeholder_message"] = "Indirizzo";
+$lang["contact_send_message"] = "Invia il messaggio";
+$lang["contact_address"] = "Invia il messaggio";
+$lang["contact_telephone"] = "Telefono";
+$lang["contact_email"] = "Email";
+/* FOOTER */
+$lang["footer_address"] = "IND";
+$lang["footer_office"] = "UFF";
+$lang["footer_email"] = "EMAIL";
+$lang["footer_newsletter"] = "Newsletter";
+$lang["footer_newsletter_description"] = "Registrati alla nostra Newsletter";
+$lang["footer_newsletter_placeholder"] = "Scrivi la tua Email";
+$lang["footer_newsletter_subscribe"] = "Iscriviti";
+/* BOTTOM FOOTER */
+$lang["footer_privacy"] = "Privacy";
+$lang["footer_cookies"] = "Cookies";
+$lang["footer_terms_conditions"] = "Terms & Conditions";
+/* CHECKOUT */
+$lang["checkout_free_shipping"] = "Spedizioni Gratuite";
+$lang["checkout_complete"] = "Ordine completato";
+$lang["checkout_product"] = "Prodotto";
+$lang["checkout_quantity"] = "Quantità";
+$lang["checkout_calculate_shipping"] = "calcola spedizione";
+$lang["checkout_total_cart"] = "Totale carrello";
+$lang["checkout_subtotal"] = "Subtotale";
+$lang["checkout_shipping"] = "Spedizione";
+$lang["checkout_continue_checkout"] = "procedi al pagamento";
+$lang["checkout_billing"] = "Dettagli di fatturazione";
+$lang["checkout_modify_shipping"] = "Modificare l'indirizzo di spedizione";
+$lang["checkout_different_address"] = "SPEDIRE AD UN INDIRIZZO DIVERSO?";
+$lang["checkout_note_order"] = "Note dell'ordine";
+$lang["checkout_your_order"] = "il tuo ordine";
+$lang["checkout_create_account"] = "Crea un'account?";
+$lang["checkout_payment_note_1"] = "Effettua il pagamento direttamente sul nostro conto bancario. Il vostro ordine non sarà spedito fino a quando i fondi vengano trasferiti nel tuo account.";
+$lang["checkout_paypal"] = "Che cosa è Paypal?";
+$lang["checkout_payment_note_2"] = "Pagare con PayPal è possibile pagare con la carta di credito se non si dispone di un conto PayPal.";
